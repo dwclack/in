4395746ae0adfe812b3fea716a3d88aea777f077
dwclack <xieshenzhiren@yeah.net>
2017-12-06 13:59:59 +0800

    审查
    审查

--- AlogMkFtp/AlogMkFtp/bin/Debug/卸载.bat
+++
@@ -1,4 +0,0 @@
-echo Ð¶ÔØAlogMkFtp.exe
-
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil /u AlogMkFtp.exe
-pause 
--- AlogMkFtp/AlogMkFtp/bin/Debug/安装.bat
+++
@@ -1,4 +0,0 @@
-echo °²×°AlogMkFtp.exe
-
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil AlogMkFtp.exe
-pause 
