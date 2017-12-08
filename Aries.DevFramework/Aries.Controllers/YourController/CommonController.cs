0513459e2626a695ade8af26cb46204dbce674e3
dwclack <xieshenzhiren@yeah.net>
2017-12-08 17:53:45 +0800

    第一个版本
    第一个版本

---
+++ Aries.DevFramework/Aries.Controllers/YourController/CommonController.cs
@@ -0,0 +1,24 @@
+﻿using Aries.Core;
+using Aries.Core.Auth;
+using Aries.Core.Config;
+using Aries.Core.DB;
+using CYQ.Data;
+using CYQ.Data.Table;
+using CYQ.Data.Tool;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Aries.Logic.Helper;
+using System.Drawing;
+
+namespace Aries.Controllers
+{
+    public partial class CommonController : Controller
+    {
+        //public System.Drawing.Bitmap VerifyCode()
+        //{
+        //    VerifyCodeHelper h = new VerifyCodeHelper();
+        //    return h.Image;
+        //}
+    }
+}
